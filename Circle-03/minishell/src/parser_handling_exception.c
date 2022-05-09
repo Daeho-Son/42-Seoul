ebf5153eb28347cadb1897a8cb83ca362224b80f
Daeho Son <dason@c3r4s7.42seoul.kr>
2022-05-09 20:36:29 +0900

    Done

--- Circle-03/minishell/src/parser_handling_exception.c
+++ Circle-03/minishell/src/parser_handling_exception.c
@@ -6,7 +6,7 @@
 /*   By: dason <dason@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 09:44:28 by dason             #+#    #+#             */
-/*   Updated: 2021/12/24 16:18:50 by dason            ###   ########.fr       */
+/*   Updated: 2022/05/09 20:34:51 by dason            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,20 @@ static bool	check_parse_error_near(t_list *list)
 		list->l_type != LTYPE_FILE && list->next->l_type != LTYPE_COMMAND && \
 		list->next->l_type != LTYPE_FILE)
 	{
-		if (list->next->l_type == 2)
+		if (list->next->l_type == LTYPE_PIPE)
 		{
 			printf("minishell: syntax error near unexpected token `|'\n");
 			return (false);
 		}
 		else if (list->l_type != LTYPE_PIPE)
 		{
-			if (list->next->l_type == 3)
+			if (list->next->l_type == LTYPE_REDIRECT_L1)
 				printf("minishell: syntax error near unexpected token `<'\n");
-			if (list->next->l_type == 4)
+			if (list->next->l_type == LTYPE_REDIRECT_L2)
 				printf("minishell: syntax error near unexpected token `<<'\n");
-			if (list->next->l_type == 5)
+			if (list->next->l_type == LTYPE_REDIRECT_R1)
 				printf("minishell: syntax error near unexpected token `>'\n");
-			if (list->next->l_type == 6)
+			if (list->next->l_type == LTYPE_REDIRECT_R2)
 				printf("minishell: syntax error near unexpected token `>>'\n");
 			return (false);
 		}
