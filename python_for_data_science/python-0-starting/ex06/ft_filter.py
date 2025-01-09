a897a5b321de305491483857bf81c3e0e0e5bfd1
Daeho-Son <thseogh135@gmail.com>
2025-01-09 13:13:24 +0900

    fix: 잘못된 yield 사용 방식을 수정

--- python_for_data_science/python-0-starting/ex06/ft_filter.py
+++ python_for_data_science/python-0-starting/ex06/ft_filter.py
@@ -4,8 +4,9 @@ def ft_filter(function, iterable):
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true."""
     if function is None:
-        yield [item for item in iterable if item]
-    yield [item for item in iterable if function(item)]
+        yield from (item for item in iterable if item)
+    else:
+        yield from (item for item in iterable if function(item))
 
 
 def main():
