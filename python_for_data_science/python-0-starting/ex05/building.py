b85da19521fc3d143a1705a5d6ac1b5c25df5ce0
Daeho-Son <thseogh135@gmail.com>
2025-01-09 09:44:55 +0900

    style: flake8로 컨벤션 수정 및 __doc__ 업데이트

--- python_for_data_science/python-0-starting/ex05/building.py
+++ python_for_data_science/python-0-starting/ex05/building.py
@@ -1,51 +1,70 @@
 import sys
 
-def main():
-	"""
-	# building.py
-	- Features: A function that receives a single string through program arguments and displays the sum of uppercase, lowercase, punctuation, numbers, and spaces.
-	- If the string is not provided through the program arguments, you must enter the string directly.
-    - Output [AssertionError] if there are two or more strings entered as program parameters.
-	"""
 
-	args = sys.argv
-	if len(args) > 2:
-		print("[AssertionError] Please enter no more than one argument.")
-		return
-	if len(args) == 2:
-		input_str = args[1]
-		get_string_info(input_str)
-	else:
-		input_str = input("What is the text to count?\n")
-		get_string_info(input_str)
+def main():
+    """
+    # building.py
+    Analyze the string and output the information below.
+    - Length of string
+    - Number of uppercase letters
+    - the number of lowercase letters
+    - the number of punctuation marks
+    - Number of spaces
+    - Number of digits
+
+    Enter the numbers through the program argument.
+    However, if there is no program argument, the string is entered directly.
+
+    If there are many program arguments, output AssertionError.
+    """
+
+    args = sys.argv
+    if len(args) > 2:
+        print("[AssertionError] Please enter no more than one argument.")
+        return
+    if len(args) == 2:
+        input_str = args[1]
+        get_string_info(input_str)
+    else:
+        while True:
+            input_str = sys.stdin.readline()
+            if input_str != '':
+                get_string_info(input_str)
+                break
+            print("Please enter a string")
+
 
 def get_string_info(string):
+    """
+    Functions for string analysis. After analyzing, output the content.
 
-	length = len(string)
+    - Parameters: String you want to analyze
+    """
 
-	upper_letters_count = 0
-	lower_letters_count = 0
-	punctuation_marks_count = 0
-	spaces_count = 0
-	digits_count = 0
-	for c in string:
-		try:
-			if 'A' <= c <= 'Z':
-				upper_letters_count += 1
-			elif 'a' <= c <= 'z':
-				lower_letters_count += 1
-			elif c == ' ':
-				spaces_count += 1
-			elif 0 <= int(c) <= 9:
-				digits_count += 1
-		except:
-			punctuation_marks_count += 1
-	print(f"The text contains {len(string)} characters:")
-	print(f"{upper_letters_count} upper letters")
-	print(f"{lower_letters_count} lower letters")
-	print(f"{punctuation_marks_count} punctuation marks")
-	print(f"{spaces_count} spaces")
-	print(f"{digits_count} digits")
+    upper_letters_count = 0
+    lower_letters_count = 0
+    punctuation_marks_count = 0
+    spaces_count = 0
+    digits_count = 0
+    for c in string:
+        try:
+            if 'A' <= c <= 'Z':
+                upper_letters_count += 1
+            elif 'a' <= c <= 'z':
+                lower_letters_count += 1
+            elif c == ' ':
+                spaces_count += 1
+            elif 0 <= int(c) <= 9:
+                digits_count += 1
+        except Exception:
+            punctuation_marks_count += 1
+    print(f"The text contains {len(string)} characters:")
+    print(f"{upper_letters_count} upper letters")
+    print(f"{lower_letters_count} lower letters")
+    print(f"{punctuation_marks_count} punctuation marks")
+    print(f"{spaces_count} spaces")
+    print(f"{digits_count} digits")
+
 
 if __name__ == '__main__':
-	main()
+    main()
