6126c882143c5915e2e82a292cc04f1f014345cf
Daeho-Son <thseogh135@gmail.com>
2025-01-09 11:57:26 +0900

    test: 테스트 코드 작성
    [테스트 코드]
    - ft_filter.__doc__
    - 기본 제공 테스트

---
+++ python_for_data_science/python-0-starting/ex06/doc_tester.py
@@ -0,0 +1,6 @@
+import ft_filter
+
+
+ft_filter_doc = ft_filter.ft_filter.__doc__
+filter_doc = filter.__doc__
+print (ft_filter_doc == filter_doc)
---
+++ python_for_data_science/python-0-starting/ex06/ex06_tester.sh
@@ -0,0 +1,33 @@
+source ../run_test.sh
+
+# ft_filter.__doc__ Test
+echo "========= TEST ft_filter.__doc ========="
+echo "Result: " $(python doc_tester.py)
+
+
+# TEST CASE 1
+case1_command="python filterstring.py 'Hello the World' 4"
+case1_expected="['Hello', 'World']"
+
+func_run_test "$case1_command" "$case1_expected"
+
+
+# TEST CASE 2
+case2_command="python filterstring.py 'Hello the World' 99"
+case2_expected="[]"
+
+func_run_test "$case2_command" "$case2_expected"
+
+
+# TEST CASE 3
+case3_command="python filterstring.py 3 'Hello the World'"
+case3_expected="AssertionError: the arguments are bad"
+
+func_run_test "$case3_command" "$case3_expected"
+
+
+# TEST CASE 4
+case4_command="python filterstring.py"
+case4_expected="AssertionError: the arguments are bad"
+
+func_run_test "$case4_command" "$case4_expected"
