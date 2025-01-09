8c7aed345e3ead419dd67e0b1fc56b516c155a9a
Daeho-Son <thseogh135@gmail.com>
2025-01-09 17:37:29 +0900

    update: 명세서에 맞게 코드 수정

--- python_for_data_science/python-0-starting/ex05/building.py
+++ python_for_data_science/python-0-starting/ex05/building.py
@@ -19,51 +19,53 @@ def main():
     """
 
     args = sys.argv
-    if len(args) > 2:
-        print("[AssertionError] Please enter no more than one argument.")
-        return
-    if len(args) == 2:
-        input_str = args[1]
-        get_string_info(input_str)
-    else:
-        while True:
-            input_str = sys.stdin.readline()
-            if input_str != '':
-                get_string_info(input_str)
-                break
-            print("Please enter a string")
+
+    try:
+        assert len(args) <= 2, \
+            "[AssertionError] Please enter no more than one argument."
+
+        if len(args) == 2:
+            input_str = args[1]
+            get_string_info(input_str)
+        else:
+            while True:
+                print("What is the text to count?")
+                input_str = sys.stdin.readline()
+                if input_str != '':
+                    get_string_info(input_str)
+                    return
+                print("Please enter a string")
+    except Exception as e:
+        print(e)
 
 
-def get_string_info(string):
+def get_string_info(input_string):
     """
     Functions for string analysis. After analyzing, output the content.
 
     - Parameters: String you want to analyze
     """
 
-    upper_letters_count = 0
-    lower_letters_count = 0
-    punctuation_marks_count = 0
-    spaces_count = 0
-    digits_count = 0
-    for c in string:
-        try:
-            if 'A' <= c <= 'Z':
-                upper_letters_count += 1
-            elif 'a' <= c <= 'z':
-                lower_letters_count += 1
-            elif c == ' ':
-                spaces_count += 1
-            elif 0 <= int(c) <= 9:
-                digits_count += 1
-        except Exception:
-            punctuation_marks_count += 1
-    print(f"The text contains {len(string)} characters:")
-    print(f"{upper_letters_count} upper letters")
-    print(f"{lower_letters_count} lower letters")
-    print(f"{punctuation_marks_count} punctuation marks")
-    print(f"{spaces_count} spaces")
-    print(f"{digits_count} digits")
+    upper_count = sum(1 for char in input_string if char.isupper())
+    lower_count = sum(1 for char in input_string if char.islower())
+    digit_count = sum(1 for char in input_string if char.isdigit())
+    space_count = sum(1 for char in input_string if char.isspace())
+    punctuation_count = sum(1 for char in input_string if is_punctuation(char))
+    print(f"The text contains {len(input_string)} characters:")
+    print(f"{upper_count} upper letters")
+    print(f"{lower_count} lower letters")
+    print(f"{punctuation_count} punctuation marks")
+    print(f"{space_count} spaces")
+    print(f"{digit_count} digits")
+
+
+def is_punctuation(c):
+    """a function of determining punctuation"""
+    ascii_value = ord(c)
+    return 33 <= ascii_value <= 47 or \
+        58 <= ascii_value <= 64 or \
+        91 <= ascii_value <= 96 or \
+        123 <= ascii_value <= 126
 
 
 if __name__ == '__main__':
